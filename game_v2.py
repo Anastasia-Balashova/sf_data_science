f3035eea831f80e3d5d9f7c577389c4c65f61cb6
Anastasia-Balashova <anastasiabalashova0@gmail.com>
2022-09-08 18:50:31 +0300

    Initial commit

---
+++ game_v2.py
@@ -0,0 +1,46 @@
+"""Игра Угадай число
+Компьютер сам загадывает и сам угадывает"""
+
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    """ Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
+    count = 0
+    
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101) # преполагаемое чило
+        if number == predict_number:
+            break
+    return(count)
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем угадывает наш алгоритмза 1000 подходов
+
+    Args:
+        randon_predict (_type_): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    
+    for number in random_array:
+        count_ls.append(random_predict(number))
+        
+    score = int(np.mean(count_ls))
+    print(f'Ваш алгоритм угадывает число в среднем за {score} попыток')
+    return(score)
+
+# RUN
+score_game(random_predict)
